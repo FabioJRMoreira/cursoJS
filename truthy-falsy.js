0522fb8928aa0cce29f945e7baa42465d1fddb1a
FabioJRMoreira <tulkas.fabio@hotmail.com>
2022-09-08 04:17:37 -0400

    comportamento alem do booleano

---
+++ truthy-falsy.js
@@ -0,0 +1,30 @@
+// boolean --> 2 valores true ou false
+const usuarioLogado = true;
+const cantaPaga = false;
+
+// truthy ou falsy
+//0 => false
+//1 => true 
+// obs === compara valor e tipo ja o == compara o valor somente 
+console.log(0==false);
+//STRING VAZIA E CONSIDERADO FALSE
+console.log(''==false);
+//1 é considerado true
+console.log(1==true);
+
+//null==> vazio ou nada
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull);
+
+let numero =3 ;
+let texto = 'Alura';
+
+console.log(typeof numero);
+console.log(typeof texto);
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
+
